218205e2d1b760d254a9c380cfb5bcb8666e6466
jayanth1947 <jayanthonteru111@gmail.com>
2023-03-31 15:42:15 +0530

    added comments for the child and parent tests

--- force-app/main/default/lwc/nestedChild/__tests__/nestedChild.test.js
+++ force-app/main/default/lwc/nestedChild/__tests__/nestedChild.test.js
@@ -1,16 +1,50 @@
 import { createElement } from 'lwc';
 import NestedChild from 'c/nestedChild';
 
+// Constants used in the tests
+const USER_DATA = {
+    Id: '1',
+    Name: 'Ram'
+};
+
+const MESSAGE = 'No user data available';
+
+// Describe block to group the test cases for the 'c-nested-child' component
 describe('c-nested-child', () => {
 
-    beforeEach(()=>{
-         const element = createElement('c-nested-child', {
-        is: NestedChild
+    // Test case to check if user data is rendered based on public property
+    test('renders user data based on public property', () => {
+
+        // Creating the 'c-nested-child' component element
+        const element = createElement('c-nested-child', { is: NestedChild });
+
+        // Setting the 'userDetail' public property of the component
+        element.userDetail = USER_DATA;
+
+        // Adding the component element to the DOM
+        document.body.appendChild(element);
+
+        // Querying the 'userName' element in the component's shadow DOM
+        const divElem = element.shadowRoot.querySelector('.userName');
+
+        // Asserting that the text content of the 'userName' element matches the expected user name
+        expect(divElem.textContent).toBe(USER_DATA.Name);
     });
-    document.body.appendChild(element);
-    })
-    
 
 
-        
+     // Test case to check if a message is rendered when user data is not available
+    test('renders message when user data is not available', () => {
+
+        // Creating the 'c-nested-child' component element
+        const element = createElement('c-nested-child', { is: NestedChild });
+
+        // Adding the component element to the DOM
+        document.body.appendChild(element);
+
+        // Querying the 'p' element in the component's shadow DOM
+        const pElement=element.shadowRoot.querySelector('.p')
+
+        // Asserting that the text content of the 'p' element matches the expected message
+        expect(pElement.textContent).toBe(MESSAGE);
+    });
 });
--- force-app/main/default/lwc/nestedParent/__tests__/nestedParent.test.js
+++ force-app/main/default/lwc/nestedParent/__tests__/nestedParent.test.js
@@ -1,29 +1,41 @@
 import { createElement } from 'lwc';
 import NestedParent from 'c/nestedParent';
 
+// Defining a constant for the expected user name
 const USER_RESULT='Ram'
 
+// Describe block to group the test cases for the 'c-nested-parent' component
 describe('c-nested-parent suite', () => {
+
+    // beforeEach block to create the component element before each test case
     beforeEach(()=>{
         const element=createElement('c-nested-parent',{
             is:NestedParent
         });
+
+        // Adding the element to the DOM
         document.body.appendChild(element)
     })
 
+     // Test case to check if the child component is rendered or not
     test('To check the child element render or not',()=>{
         const element=document.querySelector('c-nested-parent')
         const childCmp=element.shadowRoot.querySelectorAll('c-nested-child')
+
+        // Asserting that only one child component is rendered
         expect(childCmp.length).toBe(1)
     })
 
 
+    // Test case to set the user data property of the child component and check if it is rendered correctly
     test('Set the user Data Property',()=>{
         const element=document.querySelector('c-nested-parent')
         const childCmp=element.shadowRoot.querySelector('c-nested-child')
+
+        // Asserting that the user name property of the child component is set correctly
         expect(childCmp.userDetail.Name).toBe(USER_RESULT)
     })
 
 
-    
+
 });
